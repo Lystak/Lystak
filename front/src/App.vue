03d7c4a1a516764cfff24529729f6c30f3fbdb3c
Namide <damien.git@gmail.com>
2022-11-02 13:26:26 +0100

    Add return button to list

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts">
 import { RouterView } from "vue-router";
+
+const hasTouch = ('ontouchstart' in window) || (navigator.maxTouchPoints > 0)
+
+document.body.classList.add(hasTouch ? 'has-touch' : 'no-touch')
 </script>
 
 <template>
--- front/src/views/ListView.vue
+++ front/src/views/ListView.vue
@@ -22,7 +22,7 @@ const list = ref([{
   label: "Minecraft",
   description: "Java Édition",
   doneDate: 1667321949746,
-  visible: true,
+  visible: false,
   id: 654546543221,
   category: "Jeux vidéos"
 }])
@@ -35,21 +35,47 @@ const list = ref([{
 
     <div class="card bg-base-200 shadow-xl m-4 sm:m-10 gap-y-0 !mb-24">
       <div class="card-body !gap-0">
-        <h2 class="card-title mt-4 mb-8 justify-center">Cadeaux</h2>
-        <div>
-          <router-link :to="{ name: RouteName.Item, params: { itemID: item.id } }" v-for="item of list" class="flex items-center w-full border-b border-base-300 pb-3 pt-3 first:border-t gap-2">
-            <div>
-              <span class="absolute" v-if="item.doneDate > 0">✔️</span>🔲
-            </div>
-            <div class="flex-1">
+        <header class="flex items-center mt-4 mb-8">
+          <div class="flex-1">
+            <router-link
+              :to="{ name: RouteName.Home }"
+              class="btn btn-sm md:btn-md btn-ghost gap-2 normal-case lg:gap-3"
+            >
+              <svg class="h-6 w-6 fill-current md:h-8 md:w-8" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                <path d="M15.41,16.58L10.83,12L15.41,7.41L14,6L8,12L14,18L15.41,16.58Z"></path>
+              </svg>
+              <div class="flex flex-col items-start">
+                <span class="text-base-content/50 hidden text-xs font-normal md:block">Prev</span>
+                <span>Home</span>
+              </div>
+            </router-link>
+          </div>
+          <h2 class="card-title justify-center">Cadeaux</h2>
+          <span class="flex-1"></span>
+        </header>
+        <ul>
+          <li
+            class="lystak-item flex items-center relative w-full border-b border-base-300 pb-3 pt-3 first:border-t gap-2"
+            v-for="item of list"
+          >
+            <div class="flex-1" :class="{ 'line-through opacity-60': item.doneDate > 0 }">
               <strong class="font-bold block">{{ item.label }}</strong>
               <div class="flex items-center">
-                <p class="text-sm opacity-50 inline-block">{{ item.description }}</p>
+                <p class="text-sm opacity-70 inline-block">{{ item.description }}</p>
                 <span v-if="item.category" class="badge badge-primary ml-2">{{ item.category }}</span>
               </div>
             </div>
-          </router-link>
-        </div>
+            <div class="lystak-item-action absolute top-0 left-0 w-full h-full flex items-center justify-end gap-2 bg-base-200 bg-opacity-80">
+              <router-link v-if="item.visible" to="/" class="btn">Hide</router-link>
+              <router-link v-else to="/" class="btn">Show</router-link>
+
+              <router-link to="/" class="btn">Edit</router-link>
+
+              <router-link v-if="item.doneDate === 0" :to="{ name: RouteName.Item, params: { itemID: item.id } }" class="btn btn-success">Done</router-link>
+              <router-link v-else :to="{ name: RouteName.Item, params: { itemID: item.id } }" class="btn">Todo</router-link>
+            </div>
+          </li>
+        </ul>
       </div>
     </div>
 
@@ -67,5 +93,17 @@ const list = ref([{
   </section>
 </template>
 
-<style>
+<style lang="scss" scoped>
+.lystak-item-action {
+  @apply opacity-0;
+  @apply translate-y-1/2;
+  @apply pointer-events-none;
+  @apply transition-[transform,opacity]
+}
+
+:global(.no-touch .lystak-item:hover .lystak-item-action) {
+  @apply opacity-100;
+  @apply translate-y-0;
+  @apply pointer-events-auto;
+}
 </style>
